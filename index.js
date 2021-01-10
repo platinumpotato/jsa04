3b58d95d4f6f814fc27fca2448c416fa1c7cae8c
platinumpotato <58384351+platinumpotato@users.noreply.github.com>
2021-01-10 16:17:04 +0700

    10/1/2020

---
+++ index.js
@@ -0,0 +1,15 @@
+// có 10 con cừu
+// kích thước đàn cừu [12, 24, 37, 20, 14, 5, 16, 31, 27, 8]
+// mỗi tháng lượng lông mỗi con cừu tăng 10% 
+// khi lông cừu >= 50 thì cắt còn 10
+// lượng lông cắt được cho vào kho
+
+// tính lượng tiền thu được khi bán lông trong kho sau 2 năm biết 1 đơn vị lông giá 100k
+
+// 24 x {
+// cừu lớn
+
+// xem có cắt lông ko
+    // nếu có: cắt
+    // nếu ko: kệ
+// }
