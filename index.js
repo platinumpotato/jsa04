3e5ef3da49bf2279e54b1f6fd10ac53e23ceace2
platinumpotato <58384351+platinumpotato@users.noreply.github.com>
2021-01-17 14:23:49 +0700

    btvn??

--- index.js
+++ index.js
@@ -10,21 +10,21 @@
 // cừu lớn
 
 // xem có cắt lông ko
-    // nếu có: cắt
-    // nếu ko: kệ
+// nếu có: cắt
+// nếu ko: kệ
 // }
 
-var sheep = [12, 24, 37, 20, 14, 5, 16, 31, 27, 8]
+var sheep = [12, 41, 36, 54, 6, 7, 4, 5, 65, 43]
 var kho = 0
 var i = 0
-while(i<24){
-    for (let i = 0; i < sheep.length; i++){
-        sheep[i] = Math.round(sheep[i]*1.1)
-        if (sheep[i]>=50){
+while (i < 24) {
+    for (let i = 0; i < sheep.length; i++) {
+        sheep[i] = Math.round(sheep[i] * 1.2)
+        if (sheep[i] >= 50) {
             kho = kho + sheep[i] - 10
-            sheep[i]= 10
+            sheep[i] = 10
         }
     }
     i++
 }
-document.getElementById('kho').innerText = kho*100
+document.getElementById('kho').innerText = kho * 100
