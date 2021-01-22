8dca83df6d8f5a1a12c22037c2d70c696af1ecbe
platinumpotato <58384351+platinumpotato@users.noreply.github.com>
2021-01-22 13:08:10 +0700

    hw submit lesson 2

--- lesson 2/index.js
+++ lesson 2/index.js
@@ -22,11 +22,11 @@ console.log(`i have ${a} ${a===1 ? 'apple' : 'apples'}`);
 //#endregion
 
 //#region event listener
-let clicks = 0
-h1El.addEventListener('click', () => {
-    clicks++
-    h1El.innerText = `you clicked ${clicks} time(s)`
-})
+// let clicks = 0
+// h1El.addEventListener('click', () => {
+//     clicks++
+//     h1El.innerText = `you clicked ${clicks} time(s)`
+// })
 //#endregion
 
 //#region how 2 functions
@@ -46,3 +46,31 @@ h1El.addEventListener('click', () => {
 //change color button: when click, counter number change to random color
 //generate random rgb color by getting 3 random colors
 //Math.floor
+
+let color = document.getElementById('color')
+let plusBtn = document.getElementById('+')
+let minusBtn = document.getElementById('-')
+let number = document.getElementById('number')
+let numberValue = 0
+
+function numberChange() {
+    if (numberValue == 10) {
+        number.innerText = numberValue = -1
+    }
+    number.innerText = numberValue = numberValue + 1
+}
+function numberChange2() {
+    if (numberValue == 0) {
+        number.innerText = numberValue = 11
+    }
+    number.innerText = numberValue = numberValue - 1
+}
+plusBtn.addEventListener('click', numberChange)
+minusBtn.addEventListener('click', numberChange2)
+function colorChange(){
+    let blue = Math.floor(Math.random()*256)
+    let green = Math.floor(Math.random()*256)
+    let red = Math.floor(Math.random()*256)
+    number.style.color = `rgb(${red}, ${green}, ${blue})`
+}
+color.addEventListener('click', colorChange)
